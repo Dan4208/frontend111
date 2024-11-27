35a18e5a1a559a8ea6af97e306971278445228e1
royparcero30 <rparcero@gbox.ncf.edu.ph>
2024-11-27 09:10:46 +0800

    ads

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,32 @@
-import { useState } from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import About from './pages/about';
-import Home from './pages/home';
-import Contact from './pages/contact';
-import Hobbies from './pages/hobbies';
-import Education from './pages/education';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+// Import pages
+import Home from './pages/home';  
+import About from './pages/About';
+import Educ from './pages/Educ'; 
+import Hobbies from './pages/Hobbies'; 
+import Contact from './pages/contact'; 
 
+// Import components
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
-  const [count, setCount] = useState(0)
-  
-
   return (
-    
-     <Router>
-    <div className="container">
+    <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/education" element={<Education />} />
+        <Route path="/education" element={<Educ />} />
         <Route path="/hobbies" element={<Hobbies />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-    </div>
+      <Footer />
     </Router>
-   
-   
-  
-    
-   
-   
-  )
+  );
 }
 
-export default App
+export default App;
